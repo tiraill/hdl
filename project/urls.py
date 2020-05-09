33522ff1531b54dacbbdf55b3353b03342f5af14
YuriyOrlov <navuchodonsr@yandex.ru>
2020-05-09 17:33:59 +0300

    Added education basics
    - added catalog template

--- project/urls.py
+++ project/urls.py
@@ -1,9 +1,9 @@
 from django.urls import path
 
-from .views import index, living, commercial, hotels
+from .views import living, commercial, hotels
 
 urlpatterns = [
-    path('', index, name="project-index"),
+    path('', living, name="project-index"),
     path('living/', living, name="project-living"),
     path('commertial/', commercial, name="project-commercial"),
     path('hotels/', hotels, name="project-hotels"),
--- project/views.py
+++ project/views.py
@@ -1,25 +1,19 @@
 from django.shortcuts import render
 
 
-def index(request):
-
-    return render(request,
-                  template_name="project/index.html")
-
-
 def living(request):
 
     return render(request,
-                  template_name="project/index.html")
+                  template_name="project/living.html")
 
 
 def commercial(request):
 
     return render(request,
-                  template_name="project/index.html")
+                  template_name="project/commercial.html")
 
 
 def hotels(request):
 
     return render(request,
-                  template_name="project/index.html")
+                  template_name="project/hotels.html")
