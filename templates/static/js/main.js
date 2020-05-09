83c719eda733071eb5ef112668eab6aba7124a6b
YuriyOrlov <navuchodonsr@yandex.ru>
2020-05-09 17:08:45 +0300

    Repaired some templates
    - added about
    - added contacts
    - added background images

--- templates/static/js/main.js
+++ templates/static/js/main.js
@@ -240,6 +240,128 @@ $(document).ready(function () {
     ]
   });
 
+  // Keys sl
+  const
+  slidesCount = $('.keys__sl').find('.keys__sl__item').length,
+  $nav = $('.indicator'),
+  $navInner = $nav.find('.indicator__inner');
+
+  function updateNav(slide) {
+    $navInner.width(`${$nav.width() * (slide + 1) / slidesCount}px`);
+  }
+
+  $('.indicator').on('click', function(e) {
+    $('.keys__sl').slick('slickGoTo', e.offsetX / $nav.width() * slidesCount | 0);
+  });
+
+  $('.keys__sl').on({
+    init(e, slick) {
+      updateNav(slick.currentSlide);
+    },
+    beforeChange(e, slick, currentSlide, nextSlide) {
+      updateNav(nextSlide);
+    },
+  }).slick({
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 5,
+    autoplaySpeed: 3000,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 4
+        }
+      },{
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 3
+        }
+      },{
+        breakpoint: 768,
+        settings: {
+          arrows: true,
+          slidesToShow: 2,
+          prevArrow: '<button class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><filter filterUnits="userSpaceOnUse" id="Filter_0" x="0px" y="0px" width="24px" height="34px"  ><feOffset in="SourceAlpha" dx="0" dy="4" /><feGaussianBlur result="blurOut" stdDeviation="2.646" /><feFlood flood-color="rgb(96, 17, 179)" result="floodOut" /><feComposite operator="atop" in="floodOut" in2="blurOut" /><feComponentTransfer><feFuncA type="linear" slope="0.5"/></feComponentTransfer><feMerge><feMergeNode/><feMergeNode in="SourceGraphic"/></feMerge></filter></defs><g filter="url(#Filter_0)"><path fill-rule="evenodd" d="M6.184,12.586 L15.552,3.144 C15.773,2.922 16.127,2.922 16.347,3.144 C16.568,3.366 16.568,3.723 16.347,3.945 L7.378,12.984 L16.347,22.022 C16.568,22.245 16.568,22.601 16.347,22.824 C16.239,22.932 16.093,22.991 15.952,22.991 C15.810,22.991 15.665,22.937 15.557,22.824 L6.188,13.382 C5.967,13.164 5.967,12.804 6.184,12.586 Z"/></g></svg></button>',
+          nextArrow: '<button class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><filter filterUnits="userSpaceOnUse" id="Filter_1" x="0px" y="0px" width="25px" height="34px"><feOffset in="SourceAlpha" dx="0" dy="4" /><feGaussianBlur result="blurOut" stdDeviation="2.646" /><feFlood flood-color="rgb(96, 17, 179)" result="floodOut" /><feComposite operator="atop" in="floodOut" in2="blurOut" /><feComponentTransfer><feFuncA type="linear" slope="0.5"/></feComponentTransfer><feMerge><feMergeNode/><feMergeNode in="SourceGraphic"/></feMerge></filter></defs><g filter="url(#Filter_1)"><path fill-rule="evenodd" d="M16.848,12.586 L7.480,3.144 C7.259,2.922 6.905,2.922 6.684,3.144 C6.464,3.366 6.464,3.723 6.684,3.945 L15.653,12.984 L6.684,22.022 C6.464,22.245 6.464,22.601 6.684,22.824 C6.792,22.932 6.939,22.991 7.080,22.991 C7.221,22.991 7.367,22.937 7.475,22.824 L16.843,13.382 C17.064,13.164 17.064,12.804 16.848,12.586 Z"/></g></svg></button>',
+        }
+      },{
+        breakpoint: 480,
+        settings: {
+          arrows: true,
+          slidesToShow: 1,
+          prevArrow: '<button class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><filter filterUnits="userSpaceOnUse" id="Filter_0" x="0px" y="0px" width="24px" height="34px"  ><feOffset in="SourceAlpha" dx="0" dy="4" /><feGaussianBlur result="blurOut" stdDeviation="2.646" /><feFlood flood-color="rgb(96, 17, 179)" result="floodOut" /><feComposite operator="atop" in="floodOut" in2="blurOut" /><feComponentTransfer><feFuncA type="linear" slope="0.5"/></feComponentTransfer><feMerge><feMergeNode/><feMergeNode in="SourceGraphic"/></feMerge></filter></defs><g filter="url(#Filter_0)"><path fill-rule="evenodd" d="M6.184,12.586 L15.552,3.144 C15.773,2.922 16.127,2.922 16.347,3.144 C16.568,3.366 16.568,3.723 16.347,3.945 L7.378,12.984 L16.347,22.022 C16.568,22.245 16.568,22.601 16.347,22.824 C16.239,22.932 16.093,22.991 15.952,22.991 C15.810,22.991 15.665,22.937 15.557,22.824 L6.188,13.382 C5.967,13.164 5.967,12.804 6.184,12.586 Z"/></g></svg></button>',
+          nextArrow: '<button class="slick-next"><svg xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink"><defs><filter filterUnits="userSpaceOnUse" id="Filter_1" x="0px" y="0px" width="25px" height="34px"><feOffset in="SourceAlpha" dx="0" dy="4" /><feGaussianBlur result="blurOut" stdDeviation="2.646" /><feFlood flood-color="rgb(96, 17, 179)" result="floodOut" /><feComposite operator="atop" in="floodOut" in2="blurOut" /><feComponentTransfer><feFuncA type="linear" slope="0.5"/></feComponentTransfer><feMerge><feMergeNode/><feMergeNode in="SourceGraphic"/></feMerge></filter></defs><g filter="url(#Filter_1)"><path fill-rule="evenodd" d="M16.848,12.586 L7.480,3.144 C7.259,2.922 6.905,2.922 6.684,3.144 C6.464,3.366 6.464,3.723 6.684,3.945 L15.653,12.984 L6.684,22.022 C6.464,22.245 6.464,22.601 6.684,22.824 C6.792,22.932 6.939,22.991 7.080,22.991 C7.221,22.991 7.367,22.937 7.475,22.824 L16.843,13.382 C17.064,13.164 17.064,12.804 16.848,12.586 Z"/></g></svg></button>',
+        }
+      }
+    ]
+  });
+
+  // Realization sl
+  $('.realization__sl').slick({
+    arrows: true,
+    autoplay: true,
+    slidesToShow: 1,
+    autoplaySpeed: 3000,
+    dots: false,
+    adaptiveHeight: true,
+    prevArrow: '<button class="slick-prev"><svg xmlns="http://www.w3.org/2000/svg"xmlns:xlink="http://www.w3.org/1999/xlink"><path fill-rule="evenodd" d="M0.469,12.302 L11.965,0.795 C12.169,0.591 12.500,0.591 12.704,0.795 C12.908,0.999 12.908,1.331 12.704,1.535 L1.577,12.671 L12.704,23.807 C12.907,24.011 12.907,24.343 12.704,24.547 C12.601,24.648 12.467,24.700 12.334,24.700 C12.200,24.700 12.066,24.648 11.965,24.547 L0.469,13.041 C0.265,12.837 0.265,12.506 0.469,12.302 Z"/></svg></button>',
+    nextArrow: '<button class="slick-next"><svg xmlns="http://www.w3.org/2000/svg"xmlns:xlink="http://www.w3.org/1999/xlink"><path fill-rule="evenodd" d="M12.531,12.302 L1.035,0.795 C0.831,0.591 0.500,0.591 0.296,0.795 C0.092,0.999 0.092,1.331 0.296,1.535 L11.423,12.671 L0.296,23.807 C0.092,24.011 0.092,24.343 0.296,24.547 C0.399,24.648 0.532,24.700 0.666,24.700 C0.800,24.700 0.934,24.648 1.035,24.547 L12.531,13.041 C12.734,12.837 12.734,12.506 12.531,12.302 Z"/></svg></button>',
+    responsive: [
+      // {
+      //   breakpoint: 1366,
+      //   settings: {
+      //     slidesToShow: 6
+      //   }
+      // }
+    ]
+  });
+
+  // Gallery sl
+  $('.gallery__sl').slick({
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 4,
+    autoplaySpeed: 5000,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: {
+          slidesToShow: 3
+        }
+      },{
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 2
+        }
+      }
+    ]
+  });
+
+  // Showroom sl
+  $('.showroom__sl').slick({
+    arrows: false,
+    autoplay: true,
+    slidesToShow: 3,
+    autoplaySpeed: 2000,
+    dots: false,
+    responsive: [
+      {
+        breakpoint: 992,
+        settings: {
+          slidesToShow: 2
+        }
+      },{
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1
+        }
+      }
+    ]
+  });
+
 });
 
 // Map
